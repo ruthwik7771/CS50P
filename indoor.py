d9c1d8bf4bc36268be36d1a4a98e52804fa1aec1
ruthwik7771 <ruthwikchowdarymorampudi@gmail.com>
2026-05-15 20:13:14 +0530

    lowercase input method

---
+++ indoor.py
@@ -0,0 +1,19 @@
+#Ask user for input text
+text = input("What is your text? ")
+
+#lowercase the input text
+text = text.lower()
+
+#Print the output in lowercase
+print(text)
+
+"""
+Faster method to lowercase the input text
+
+Take input text and lowercase it in ine line
+text = input("What is your text? ").lower()
+
+print the output lowercase text
+print(text)
+
+"""
