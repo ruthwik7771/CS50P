6ecc272313a9dbe747847578aa2cf622434a25c1
ruthwik7771 <ruthwikchowdarymorampudi@gmail.com>
2026-05-15 20:14:46 +0530

    Implement text playback feature for given text

---
+++ playback.py
@@ -0,0 +1,20 @@
+#Ask user for input text
+text = input("What' your input? ")
+
+#Adding playbacks in between text
+text = text.replace(" ","...")
+
+#Print the Playback text
+print(text)
+
+"""
+Faster way to convert is
+
+Ask user for input text and add playbacks
+text = input("What's your input text ").replace(" ","...")
+
+printing the final converted text
+print(text)
+
+"""
+
