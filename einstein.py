4f75551317ee06864684daed7991d94e820cc9c6
ruthwik7771 <ruthwikchowdarymorampudi@gmail.com>
2026-06-01 15:56:53 +0530

    Create einstein.py

---
+++ einstein.py
@@ -0,0 +1,11 @@
+#take mass m in kilograms as integer
+m = int(input("m: "))
+
+#making c a constant
+c = 300000000
+
+#calculating equivalent energy in joules as integer
+E = m*(c**2)
+
+#printing energy equivalent
+print(f"E: {E}")
